6ec087798aeddb86f6e47813a3909ecdb74f3357
itayOkabiLavi <92581461+itayOkabiLavi@users.noreply.github.com>
2022-01-02 13:37:41 +0200

    Add files via upload

--- Server.cpp
+++ Server.cpp
@@ -1,98 +1,100 @@
 
 #include "Server.h"
-const int SOCK_ERR = -1, BIND_ERR = -2, CONNECTION_ERR = -3;
-/**
- * Notice:   "serv_addr.sin_addr.s_addr = INADDR_ANY;" may cause pproblems
- */
-Server::Server(int port)throw (const char*)  : myPort(port) {
 
+int Server::configureCommunication() {
+    // create socket
+    this->mySocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (mySocket == -1)
+    {
+        srvmsg( "Can't create a socket!" );
+        return -1;
+    }
+    srvmsg("my socket # is " + to_string(this->mySocket));
+    // bind socket to ip/port
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(this->myPort);
+    srvmsg( "my port is " + std::to_string( hint.sin_port) );
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+    int binding = bind(mySocket, (sockaddr*)&hint, sizeof(hint));
+    if (binding < 0) {
+        srvmsg( "Can't bind a socket! (" + std::to_string( binding ) + ")" );
+        return -2;
+    }
+    // telling winsock
+    int listening = listen(mySocket, SOMAXCONN);
+    if (listening < 0) {
+        srvmsg( "Can't listen! (" + std::to_string( listening ) + ")" );
+        return -3;
+    }
+    /*
+    char host[NI_MAXHOST];
+    char svc[NI_MAXSERV];*/
+    return 0;
 }
 
-void serverMessages(string msg) {
-    cout << "-> Server: " << msg << endl;
+bool Server::handleClient(int clientSocket, char* buf, int byteRcv){
+    string clientInput = std::string (buf, 0, byteRcv);
+    srvmsg("CLIENT:_" + std::string (buf, 0, byteRcv) + "_");
+    if (clientInput == EXIT_WORD) {
+        srvmsg("finished communication with " + to_string(clientSocket));
+        char okBye[] = "ok bye\n";
+        send(clientSocket, okBye, sizeof(okBye), 0);
+        return false;
+    }
+    else {
+        char gotYou[] = "got you fam\n";
+        send(clientSocket, gotYou, sizeof(gotYou), 0);
+    }
+    return true;
 }
 
-void startListening(ClientHandler &ch, int myPort, int* dstSocket) {
-    serverMessages("started listening");
-    int mySocket, clientID;
-    mySocket = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (mySocket < 0) {
-        serverMessages("error creating socket");
+void Server::listenToClient() {
+    while(!this->startCommunication);
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    int clientSocket = accept(this->mySocket, (sockaddr*)&client, &clientSize);
+    if (clientSocket < 0) {
+        srvmsg( "Can't accept! (" + std::to_string(clientSocket) + ")" );
         return;
-    } else {
-        serverMessages("socket created");
     }
-    *dstSocket = mySocket;
-    struct hostent *server = gethostbyname("localhost");
-    struct sockaddr_in serv_addr;
-    //Binding
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = myPort;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    if (bind(mySocket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        serverMessages("error binding");
-        close(mySocket);
-        return;
-    } else {
-        serverMessages("binding successful");
+    int bufSize = 4096;
+    char buf[bufSize];
+    srvmsg( "accepted client " + to_string(clientSocket));
+    std::string clientInput = "";
+    bool cont = true;
+    while (cont){
+        memset(buf, 0, bufSize);
+        int byteRcv = recv(clientSocket, buf, bufSize, 0);
+        if (byteRcv < 0) {
+            srvmsg( "connection issue" );
+            break;
+        }
+        if (byteRcv == 0) {
+            srvmsg( "client disconnected" );
+            break;
+        }
+        cont = handleClient(clientSocket, buf, byteRcv);
     }
-    //Listening
-    listen(mySocket, 15);
-    //Timeout
-    struct timeval tv;
-    tv.tv_sec = 5;
-    tv.tv_usec = 0;
-    fd_set readfds;
-    FD_ZERO(&readfds);
-    FD_SET(mySocket, &readfds);
-    int t = select(mySocket + 1, &readfds, NULL, NULL, &tv);
-    if (t == 0) {
-        serverMessages("select() error - timeout");
-        close(mySocket);
-        return;
-    } else if (t == -1){
-        serverMessages("select() error");
-        close(mySocket);
-        return;
-    } else {
-        //Accepting clients
-        //while (true) {
-            clientID = accept(mySocket, (struct sockaddr *) &serv_addr, (socklen_t *) sizeof(serv_addr));
-            if (clientID < 0) {
-                serverMessages("accepting failed");
-                close(mySocket);
-                return;
-            } else {
-                serverMessages("accepted client " + to_string(clientID) );
-                ch.handle(clientID);
-            }
-        //}
-    }
-}
-
-string now() {
-    time_t now = time(0);
-    struct tm  tstruct;
-    char nowString[80];
-    tstruct = *localtime(&now);
-    strftime(nowString, sizeof(nowString), "%X", &tstruct);
-
-    return nowString;
+    this->clientsSockets.push_back(clientSocket);
 }
 
 void Server::start(ClientHandler& ch)throw(const char*){
-    serverMessages("started " + now());
-    this->myThread = new thread (startListening, ref(ch) ,this->myPort, &this->mySocket);
-    serverMessages("finished " + now());
-    //this->myThread[0].join();
+    if (this->mySocket >= 0) {
+        this->startCommunication = true;
+        srvmsg("thread allowed to start communication");
+    }
 }
 
 void Server::stop(){
-    this->myThread[0].join();
+    this->myThread->join();
     close(this->mySocket);
-    serverMessages("stopped");
+    for (int sock = 0; sock < this->clientsSockets.size(); ++sock) {
+        close(this->clientsSockets.at(sock));
+    }
+    srvmsg("stopped and cleared everything");
 }
 
 Server::~Server() {
 }
+
--- Server.h
+++ Server.h
@@ -9,14 +9,17 @@
 #define SERVER_H_
 
 #include <sys/socket.h>
-#include <thread>
 #include <iostream>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <string.h>
 #include <time.h>
 #include <vector>
+#include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <string>
+#include <thread>
 
 using namespace std;
 
@@ -38,19 +41,31 @@ class AnomalyDetectionHandler:public ClientHandler{
     }
 };
 
-
 // implement on Server.cpp
 class Server {
-    thread *myThread; // my try
+    thread *myThread;
+    bool startCommunication;
     int myPort;
-	// you may add data members
-public:
     int mySocket;
-	Server(int port) throw (const char*);
+    vector<int> clientsSockets = {};
+    const string EXIT_WORD = "\n"; // also in mainTrain.cpp
+	// you may add data members
+    void srvmsg(std::string msg) {
+        std::cout << "SRV->" << msg << std::endl;
+    }
+    void listenToClient();
+public:
+	Server(int port) throw (const char*) : myPort(port) {
+        this->configureCommunication();
+        this->startCommunication = false;
+        this->myThread = new thread(&Server::listenToClient, this);
+    }
 	virtual ~Server();
     //void startListening(ClientHandler &ch ,int myPort);
     void start(ClientHandler& ch)throw(const char*);
-	void stop();
+    int configureCommunication();
+    bool handleClient(int clientSocket, char* buf, int byteRcv);
+    void stop();
 };
 
 #endif /* SERVER_H_ */
